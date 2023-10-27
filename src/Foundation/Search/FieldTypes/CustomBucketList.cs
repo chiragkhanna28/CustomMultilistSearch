114d44cb6f25a37a0d0e61283251c24f6e4655d8
Chirag Khanna <ckhanna@horizontal.com>
2023-10-28 01:09:34 +0530

    multilist field with search change

--- src/Foundation/Search/FieldTypes/CustomBucketList.cs
+++ src/Foundation/Search/FieldTypes/CustomBucketList.cs
@@ -1,41 +1,18 @@
-﻿using Microsoft.Extensions.DependencyInjection;
-using Sitecore;
-using Sitecore.Buckets.Extensions;
+﻿using Sitecore.Buckets.Extensions;
 using Sitecore.Buckets.FieldTypes;
-using Sitecore.Buckets.Util;
 using Sitecore.ContentSearch.Utilities;
 using Sitecore.Data.Items;
-using Sitecore.DependencyInjection;
-using Sitecore.Diagnostics;
-using Sitecore.Globalization;
-using Sitecore.Resources;
-using System;
-using System.Collections.Generic;
-using System.Collections.Specialized;
-using System.Linq;
-using System.Web;
-using System.Web.UI;
 
 namespace Search.FieldTypes
 {
-    public class CustomBucketList : SearchList
+    public class CustomBucketList : BucketList
     {
-        /// <summary>Gets parameters for JavaScript initialize function</summary>
         protected override string ScriptParameters
         {
             get
             {
                 return string.Format("'{0}'", (object)string.Join("', '", (object)this.ID, (object)this.ClientID, (object)this.PageNumber, (object)"/sitecore/shell/Applications/Buckets/Services/Search.ashx", (object)this.Filter, (object)SearchHelper.GetDatabaseUrlParameter("&"), (object)this.TypeHereToSearch, (object)this.Of, (object)this.EnableSetNewStartLocation));
             }
-        }
-
-        /// <summary>Render start location input.</summary>
-        /// <param name="output">Output stream.</param>
-        protected override void RenderStartLocationInput(HtmlTextWriter output)
-        {
-            if (!this.EnableSetNewStartLocation)
-                return;
-            base.RenderStartLocationInput(output);
         }
 
         public override string OutputString(Item item)
